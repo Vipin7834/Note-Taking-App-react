020166145b020a51eecaf2832eb382096d019211
Vipin7834 <vipinkushwaha7834@gmail.com>
2023-06-24 13:09:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Note Taking Application
+
+
+
+
+
+
+## Description
+
+ This simple note taking application allows you to save notes with a title and plain text . The Note Taking app makes creating, viewing, and deleting notes and Edit notes!
+
+
+## 🔗 Deployment Application Links
+
+https://gilded-queijadas-0e3983.netlify.app/
+
+
+## Installation
+
+Install Note-Taking-App with npm
+
+```bash
+  npm install Note-Taking-App
+  cd Note-Taking-App
+```
+Run npm install to install all dependencies. To use the application locally, run node server.js, and then open http://localhost:3000 in your preferred browswer. The Note Taking App is live.
+    
---
+++ src/pages/Notes.js
@@ -0,0 +1,48 @@
+// import React, { useEffect } from 'react'
+import {CiSearch} from 'react-icons/ci'
+import {MdClose} from 'react-icons/md'
+// import dummyNotes from '../dummy_notes'
+import { Link } from 'react-router-dom'
+import {BsPlusLg} from 'react-icons/bs'
+
+import NoteItem from '../components/NoteItem'
+import {useState} from 'react'
+
+
+const Notes = ({notes}) => {
+  const[showSearch, setShowSearch]= useState(false);
+  const [text, setText]= useState('')
+  const [filteredNotes, setFilteredNotes] = useState(notes)
+
+ const handleSearch = ()=>{
+setFilteredNotes(notes.filter(note =>{
+  if(note.title.toLowerCase().match(text.toLowerCase())){
+    return note;
+  }
+}))
+ }
+
+
+//  useEffect(handleSearch, [text]) 
+
+
+  return (
+<section>
+    <header className="notes__header">
+       {!showSearch && <h2>Notes...</h2>}
+       {showSearch &&  <input type="text" value={text} onChange={(e) => {setText(e.target.value); handleSearch();}} autoFocus  placeholder='keyword...' />}
+        <button className='btn' onClick={()=>setShowSearch(prevState => !prevState)} >{showSearch ?<MdClose/> : <CiSearch/>}</button>
+    </header>
+
+    <div className="notes__container">
+      {filteredNotes.length === 0 && <p className='empty_notes'>Not notes found.</p>}
+        {
+            filteredNotes.map(note => <NoteItem key={note.id} note= {note} />)
+        }
+    </div>
+    <Link to="/create-note" className='btn add__btn'><BsPlusLg/></Link>
+</section>
+  )
+}
+
+export default Notes
